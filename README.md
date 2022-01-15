0e81241f7c980196fbdf6c7ab205775a65ce2726
smartdev0322 <smartdev0322@outlook.com>
2022-01-15 05:46:57 -0600

    complete

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # devprojects-flask-google-login
+
+## Techs
+
+- Flask, requests, oauthlib, pyOpenSSL, Flask-Login
+
+## Refs
+
+- Project tutorial
+[Create a flask application with google login](https://realpython.com/flask-google-login/#creating-your-own-web-application)
+- GitHub ref project
+[flask-google-login](https://github.com/realpython/materials/tree/master/flask-google-login)
+
+## Main dependancies
+
+- python3.7
+- requests==2.21.0
+- Flask==1.0.2
+- oauthlib==3.0.1
+- pyOpenSSL==19.0.0
+- Flask-Login==0.4.1
+
+## Prepare development environment
+
+- Install python3.7
+    ```sh
+    $ sudo add-apt-repository ppa:deadsnakes/ppa
+    $ sudo apt-get update
+    $ sudo apt-get install python3.7
+    ```
+- Install pip3
+    ```sh
+    $ sudo apt update
+    $ sudo apt install python3-pip
+    ```
+- Install venv for python3.7
+    ```sh
+    $ sudo apt-get install -y python3.7-venv
+    ```
+- Create virtual environment using venv & activate
+    ```sh
+    $ python3.7 -m venv env
+    $ source env/bin/activate
+    ```
+- Install requirements
+    ```sh
+    $ pip install -r requirements.txt
+    ```
+
+## Run project
+
+### Set your client credentials as environmental variables
+
+- Linux bash terminal and Mac OS X terminal
+
+	```bash
+	export GOOGLE_CLIENT_ID=your_client_id
+	export GOOGLE_CLIENT_SECRET=your_client_secret
+	```
+
+- If you’re on Windows, in Command Prompt.
+	```bash
+	set GOOGLE_CLIENT_ID=your_client_id
+	set GOOGLE_CLIENT_SECRET=your_client_secret
+	```
+
+### Run
+
+    ```sh
+    $ source env/bin/activate
+    (env)$ python app.py
+    ```
+
+    Navigate to <https://localhost:5000>
---
+++ app.py
@@ -0,0 +1,172 @@
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+# Flask app setup
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+    # https://accounts.google.com/o/oauth2/v2/auth
+    
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    # request_uri will look like this
+    # https://accounts.google.com/o/oauth2/v2/auth
+    # ?response_type=code
+    # &client_id=850878184963-e544te41583q8lnndo567d7ob3il1mab.apps.googleusercontent.com
+    # &redirect_uri=https%3A%2F%2F127.0.0.1%3A5000%2Flogin%2Fcallback
+    # scope=openid+email+profile
+    
+    return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+    # code will look like this
+    # 4/0AX4XfWh2886VN0LrwtmyP_D1Ma0yBlOnPhd_ceEfBoA2ks_JMcHzlRxWD8lUz4o4nOBI3Q
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    print("google_provider_cfg", google_provider_cfg)
+    print("token_endpoint", token_endpoint)
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    print(token_url, headers, body)
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+    print(token_response)
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    print(userinfo_endpoint, uri, headers, body, userinfo_response)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in our db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+    print(user)
+
+    # Doesn't exist? Add to database
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
